77106fee2930b5a6b6a6e4965c5ba834c1900691
JPesoa1 <josepesoa002@gmail.com>
2022-12-27 12:18:08 +0100

    Refactorizando Adapter Home

--- app/src/main/java/com/example/blogappdemo/ui/home/adapter/HomeScreenAdapter.kt
+++ app/src/main/java/com/example/blogappdemo/ui/home/adapter/HomeScreenAdapter.kt
@@ -30,21 +30,37 @@ class HomeScreenAdapter (private val  postList: List<Post>):RecyclerView.Adapter
 
     private inner class HomeScreenViewHolder(val binding:PostItemViewBinding,val context: Context):BaseViewHolder<Post>(binding.root){
         override fun bind(item: Post) {
-            Glide.with(context).load(item.post_image).centerCrop().into(binding.postImage)
-            Glide.with(context).load(item.profile_picture).centerCrop().into(binding.profilePicture)
-            binding.profileName.text=item.profile_name
 
-            if (item.post_description.isEmpty()){
-                binding.postDescription.hide()
-            }else{
-                binding.postDescription.text=item.post_description
-            }
+            setupProfileInfo(item)
+            addPostTimeStamp(item)
+            setupPostImage(item)
+            setupPostDescription(item)
 
-            val createdAt = (item.createdAt?.time?.div(1000L))?.let{
+        }
+
+        private  fun setupProfileInfo(post:Post){
+            Glide.with(context).load(post.profile_picture).centerCrop().into(binding.profilePicture)
+            binding.profileName.text=post.profile_name
+        }
+
+        private  fun addPostTimeStamp(post:Post){
+            val createdAt = (post.createdAt?.time?.div(1000L))?.let{
                 TimeUtils.getTimeAgo(it.toInt())
             }
             binding.postTimestamp.text=createdAt
+        }
 
+        private fun setupPostImage(post:Post){
+            Glide.with(context).load(post.post_image).centerCrop().into(binding.postImage)
+        }
+
+        private fun setupPostDescription(post:Post){
+
+            if (post.post_description.isEmpty()){
+                binding.postDescription.hide()
+            }else{
+                binding.postDescription.text=post.post_description
+            }
         }
     }
 
